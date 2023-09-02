3739ccdee7cf66f116199117502afc5808b46873
Juan Fernando <jawuat@gmail.com>
2023-09-01 20:11:11 -0500

    cambio data

--- instagram_graph_api/defines.php
+++ instagram_graph_api/defines.php
@@ -2,7 +2,7 @@
 	session_start();
 
 	define( 'FACEBOOK_APP_ID', '2532904296869709' );
-	define( 'FACEBOOK_APP_SECRET', '●●●●●●●●' );
+	define( 'FACEBOOK_APP_SECRET', '5522aa1ea913e71e65690d47193bc8ce' );
 	define( 'FACEBOOK_REDIRECT_URI', 'https://jawuatfdiaz.github.io/instagramApiGraph/instagram_graph_api/obtaining_access_token.php' );
 	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v5.0/' );
 
