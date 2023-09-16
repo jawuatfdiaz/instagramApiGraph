7377c390f4bc3743294c40eec8bd70096b28a644
Juan Fernando Diaz Polo <jdiazpol@emeal.nttdata.com>
2023-09-15 23:23:48 -0500

    update

--- instagram_graph_api/js/script.js
+++ instagram_graph_api/js/script.js
@@ -26,3 +26,20 @@
         .catch(error => {
             console.error("Error al cargar el formulario de inicio de sesión:", error);
         });
+
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '2532904296869709',
+            xfbml      : true,
+            version    : 'v17.0'
+          });
+          FB.AppEvents.logPageView();
+        };
+      
+        (function(d, s, id){
+           var js, fjs = d.getElementsByTagName(s)[0];
+           if (d.getElementById(id)) {return;}
+           js = d.createElement(s); js.id = id;
+           js.src = "https://connect.facebook.net/en_US/sdk.js";
+           fjs.parentNode.insertBefore(js, fjs);
+         }(document, 'script', 'facebook-jssdk'));
